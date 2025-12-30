919080f22f4c983dac896803c4ddf6e2a64880dc
Chris-Libra-Unipd <christian.libralato@studenti.unipd.it>
2025-12-30 13:04:20 +0100

    modifiche accessibilità

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -5,10 +5,12 @@ function showHidePsw(){
     if(field.type == "password"){
         document.getElementById("showHideIcon").src="assets/icons/hidePsw.png";
         field.type = "text";
+        document.getElementById("showHidePsw").setAttribute("aria-label","nascondi password");
     }
     else if(field.type == "text"){
         document.getElementById("showHideIcon").src="assets/icons/showPsw.png";
         field.type = "password";
+        document.getElementById("showHidePsw").setAttribute("aria-label","mostra password");
     }
 }
 
